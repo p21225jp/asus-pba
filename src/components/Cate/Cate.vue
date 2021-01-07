581b1aa93bd0d1d0050175eed1e535073ad15fe9
Jack1 Yan(严文杰_华硕上海) <Jack1_Yan@acc.corpnet.asus>
2021-01-07 12:48:55 +0800

    add gundam and bg

--- src/components/Cate/Cate.vue
+++ src/components/Cate/Cate.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="Cate" v-if="goods">
 			<nut-row>
+				<div class="gundam gundam-zagu"><img src="./zagu.png" alt=""></div>
+				<div class="gundam gundam-rx78"><img src="./rx78-2.png" alt=""></div>
 				<router-link :to="'/goods/'+index" v-for="(item,index) in goods" :key = "index">
 				    <nut-col :span="24">
 				    	<img :src="item.imgurl" alt="">
@@ -33,11 +35,23 @@
 	}
 </script>
 <style scoped>
+	.gundam {
+		position: absolute;
+	}
+	.gundam-zagu {
+		left: 10%;
+    	top: 230px;
+	}
+	.gundam-rx78 {
+		right: 7%;
+    	top: 125px;
+	}
 	.Cate {
 		width:100%;
 		height: 1920px;
 		background-size: 100%;
-		background-image:url('https://img11.360buyimg.com/cms/jfs/t1/137727/15/19975/386801/5fe402d7E532e151e/8e6ece2fe3d09090.jpg');
+		background-image:url('./bg.jpg');
+		position: relative;
 	}
 	.nut-row {
 		padding-top:150px;
