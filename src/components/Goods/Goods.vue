c9dd86f6ffa7ea41db9698cc42d47680710ba349
Jack1 Yan(严文杰_华硕上海) <Jack1_Yan@acc.corpnet.asus>
2021-03-16 14:14:46 +0800

    modify goods

--- src/components/Goods/Goods.vue
+++ src/components/Goods/Goods.vue
@@ -1,11 +1,18 @@
 <template>
 	<div class="contain" v-if="goods_info[$route.params.id]">
-		<nut-tab position-nav="bottom">
+		<nut-drag @click.native="goBack">
+		  	<div class="touch-dom"><img src="//img11.360buyimg.com/cms/jfs/t1/165243/22/14390/1711/605048faE37859777/eb03580a3eb8a792.png" alt=""></div>
+		</nut-drag>
+		<nut-swiper :paginationVisible="true" direction="horizontal" :autoPlay="5000">
+			<div  v-for="(item,index) in goods_info[$route.params.id].goods" :key="index"  class="nut-swiper-slide">
+		        <img :src="item.img" alt="">
+		    </div>
+		</nut-swiper>
+		<!-- <nut-tab position-nav="left" :is-scroll="true">
 		    <nut-tab-panel v-for="(item,index) in goods_info[$route.params.id].goods" :tab-title="item.name" :key="index">
 		    	<img :src="item.img" alt="">
 			</nut-tab-panel>
-		</nut-tab>
-		<nut-navbar @on-click-back="goBack" :rightShow="false"><a class="fanhui" slot="left" @click="goBack">返回</a>{{goods_info[$route.params.id].title}}</nut-navbar>
+		</nut-tab> -->
 	</div>
 </template>
 <script>
@@ -39,18 +46,35 @@
 		width: 100%;
 		height: 100%;
 	}
+	.contain .top-banner {
+		width: 1080px;
+		height: 345px;
+	}
 	.nut-tab {
 		padding: 0;
+		border: none;
+
+	}
+	.nut-drag {
+		left: 18px;
+		top: 612px;
+	}
+	.nut-drag .touch-dom {
+		color: #fff;
 	}
 	.nut-navbar {
 	    height: 50px;
 	    line-height: 50px;
-	    position: fixed;
-	    bottom: 0;
+	    top: 0;
 	    width: 100%;
+	    background: #eee;
+	}
+	.nut-tab-title-leftnav {
+		width: 279px;
 	}
 	.nut-navbar .fanhui {
 		vertical-align: middle;
+		font-size: 18px;
 	}
 	.nut-navbar .nav-center .nav-title {
 		font-size: 2em;
@@ -62,8 +86,11 @@
 		height: 100%;
 		padding: 0;
 	}
-	.nut-tab-title-bottomnav {
-		position: fixed;
-    	bottom: 49px;
+	.nut-swiper {
+		height: 1920px;
+	}
+	.nut-swiper .nut-swiper-pagination .swiper-pagination-bullet {
+		width: 16px;
+		height: 16px;
 	}
 </style>
